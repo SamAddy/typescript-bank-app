06f6ab073e303477339160c0b236520e7148d2b0
SamAddy <addisonsamuel70@yahoo.com>
2023-04-27 17:30:56 +0300

    Add more testing instances

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,28 @@ const westBranch = new Branch("West Branch")
 const sunBranch = new Branch("Sun Branch")
 const customer1 = new Customer("John")
 const customer2 = new Customer("Anna")
-const customer3 = new Customer("John")
+const customer3 = new Customer("Jim Ike")
 
 arizonaBank.addBranch(westBranch)
 arizonaBank.addBranch(sunBranch)
 arizonaBank.addBranch(westBranch) 
 
-arizonaBank.findBranchByName("bank")
-arizonaBank.findBranchByName("sun")
+
+
+arizonaBank.findBranchByName("Sun") // returns [Branch: "name": "Sun Branch", "customers": [] }] 
+arizonaBank.findBranchByName("bank") // returns []
 
 arizonaBank.addCustomer(westBranch, customer1)
 arizonaBank.addCustomer(westBranch, customer3)
 arizonaBank.addCustomer(sunBranch, customer1)
 arizonaBank.addCustomer(sunBranch, customer2)
 
-arizonaBank.addCustomerTransaction(westBranch, customer1.getId, 3000)
-arizonaBank.addCustomerTransaction(westBranch, customer1.getId, 2000)
-arizonaBank.addCustomerTransaction(westBranch, customer2.getId, 3000)
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), { amount: 3000, date: new Date() }) // returns "customer-1 made new transaction 3000"
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), { amount: 2000, date: new Date() }) // returns "customer-1 made new transaction 2000"
+arizonaBank.addCustomerTransaction(sunBranch, customer2.getId(), { amount: 3000, date: new Date() })
+arizonaBank.addCustomerTransaction(westBranch, customer3.getId(), { amount: 3000, date: new Date() })
 
-// customer1.addTransactions(-1000)
-// console.log(customer1.getBalance())
-// console.log(arizonaBank.listCustomers(westBranch, true))
-// console.log(arizonaBank.listCustomers(sunBranch,true))
+customer1.addTransactions({ amount: -1000, date: new Date() })
+console.log(customer1.getBalance())
+console.log(arizonaBank.listCustomers(westBranch, true))
+console.log(arizonaBank.listCustomers(sunBranch, false))
